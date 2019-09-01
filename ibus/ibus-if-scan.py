d39161c8a6e297e1bbfc11e110cd136321a6c151
Norimitsu Sugimoto <dictoss@live.jp>
2019-09-01 17:13:26 +0900

    print property and parameter.

--- ibus/ibus-if-scan.py
+++ ibus/ibus-if-scan.py
@@ -10,6 +10,19 @@ import dbus
 
 _IS_VERBOSE = False
 
+class DbusProperty(object):
+    def __init__(self, name, vertype, access):
+        self.name = name
+        self.vertype = vertype
+        self.access = access
+
+
+class DbusMethodArg(object):
+    def __init__(self, name, vertype, direction):
+        self.name = name
+        self.vertype = vertype
+        self.direction = direction
+
 
 def _debug_print(s):
     if _IS_VERBOSE:
@@ -74,12 +87,48 @@ def scan_dbus_interface_for_ibus():
         print("interface: %s" % (o.get("name")))
 
         _props = o.findall("property")
+        _proplist = []
         for p in _props:
-            print("  property: %s" % (p.get("name")))
+            _tmp_prop = DbusProperty(p.get("name"), p.get("type"), p.get("access"))
+            _proplist.append(_tmp_prop)
+
+        # print property
+        if 0 < len(_proplist):
+            print("  PROPERTY:")
+            for a in _proplist:
+                print("  %02s | %s | %s" % (a.vertype, a.name, a.access))
+
 
         _methods = o.findall("method")
         for m in _methods:
             print("  method: %s" % (m.get("name")))
+
+            _args = m.findall("arg")
+            _in_args = []
+            _out_args = []
+            for a in _args:
+                # direction: in/out
+                # name:
+                # type: parameter type(string, object, value, binary)
+                _tmp_arg = DbusMethodArg(a.get("name"), a.get("type"), a.get("direction"))
+
+                if a.get("direction") == "in":
+                    _in_args.append(_tmp_arg)
+                elif a.get("direction") == "out":
+                    _out_args.append(_tmp_arg)
+                else:
+                    raise Exception("ERROR: unknown direction")
+
+            # print in-parameter
+            if 0 < len(_in_args):
+                print("    IN  ARGS")
+                for a in _in_args:
+                    print("      %02s | %s" % (a.vertype, a.name))
+
+            if 0 < len(_out_args):
+                print("    OUT ARGS")
+                for a in _out_args:
+                    print("      %02s | %s" % (a.vertype, a.name))
 
 
 if '__main__' == __name__:
